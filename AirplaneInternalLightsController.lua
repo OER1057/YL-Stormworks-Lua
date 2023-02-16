65702abcb51490bd4a09896562aaed5ca6eacd7b
OER1057 <96519093+OER1057@users.noreply.github.com>
2023-02-16 15:21:12 +0900

    照明操作盤コードを微修正

--- AirplaneInternalLightsController.lua
+++ AirplaneInternalLightsController.lua
@@ -11,7 +11,7 @@ do
     ---@type Simulator -- Set properties and screen sizes here - will run once when the script is loaded
     simulator = simulator
     simulator:setScreen(1, "2x1")
-    simulator:setProperty("NAVI Default State", true)
+    simulator:setProperty("CKPT Default State", true)
     -- Runs every tick just before onTick; allows you to simulate the inputs changing
     ---@param simulator Simulator Use simulator:<function>() to set inputs etc.
     ---@param ticks     number Number of ticks since simulator started
@@ -35,16 +35,24 @@ require("MonitorButton")
 
 buttons = {}
 
-buttonName = { "NAVI", "LOGO", "BCON", "TAXI", "STRB", "LNDG" }
-buttonWidth = 19
-buttonHeight = 5
+buttonName = { "CKPT", "CABN", "BLT", "SMK", "EXT" }
 for index, name in ipairs(buttonName) do
-    x = 8 + 29 * ((index - 1) % 2)
-    y = 11 + 7 * math.floor((index - 1) / 2)
+    if index <= 2 then
+        x = 8 + 29 * (index - 1)
+        y = 11
+        buttonWidth = 19
+        buttonHeight = 5
+    else
+        x = 5 + 20 * (index - 3)
+        y = 25
+        buttonWidth = 14
+        buttonHeight = 5
+    end
     TouchArea:addArea(name, x, y, buttonWidth, buttonHeight)
     defaultState = property.getBool(name .. " Default State")
     buttons[name] = ToggleButton.new(defaultState, 1, 1)
 end
+
 
 function onTick()
     isTouched = input.getBool(1)
@@ -54,20 +62,20 @@ function onTick()
     for name, button in pairs(buttons) do
         button:update(TouchArea.whichArea == name)
     end
-    output.setBool(1, buttons["NAVI"].isOn)
-    output.setBool(2, buttons["LOGO"].isOn)
-    output.setBool(3, buttons["BCON"].isOn)
-    output.setBool(4, buttons["TAXI"].isOn)
-    output.setBool(5, buttons["STRB"].isOn)
-    output.setBool(6, buttons["LNDG"].isOn)
+    output.setBool(1, buttons["CKPT"].isOn)
+    output.setBool(2, buttons["CABN"].isOn)
+    output.setBool(3, buttons["BLT"].isOn)
+    output.setBool(4, buttons["SMK"].isOn)
+    output.setBool(5, buttons["EXT"].isOn)
 end
 
 function onDraw()
     black()
     screen.drawClear()
     bwhite()
-    screen.drawText(8, 1, "EXT");
+    screen.drawText(8, 1, "INT");
     screen.drawText(27, 1, "LIGHTS")
+    screen.drawText(20, 18, "SIGNS")
     for name, button in pairs(buttons) do
         bwhite()
         screen.drawText(TouchArea.areaList[name].xStart, TouchArea.areaList[name].yStart, name)
